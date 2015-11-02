1145e935de735db6de24b5ae567e10b5b40d0e6f
Ben <ben.simmons@trinityis.com>
2015-11-01 20:12:36 -0600

    Initial Commit

---
+++ BlocBrowser/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BlocBrowser
+//
+//  Created by Benjamin Simmons on 11/1/15.
+//  Copyright © 2015 Ben Simmons. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
